703a91b20992bdf0a6b73e347599bb35e596e353
Aarne Wallén <aarne.wallen@siili.com>
2022-06-07 11:41:02 +0300

    Breadcrumb link fixes and styling.

--- src/components/navigation/Breadcrumb.tsx
+++ src/components/navigation/Breadcrumb.tsx
@@ -1,21 +1,38 @@
-import { DrupalMenuLinkContent } from "next-drupal";
-import { ReactElement } from "react";
+import classNames from "@/lib/classNames"
+import { IconAngleRight, IconArrowRight } from "hds-react"
+import { DrupalMenuLinkContent } from "next-drupal"
+import { ReactElement } from "react"
+import { useTranslation } from 'next-i18next'
 
 import styles from './navigation.module.scss'
+import Link from "next/link"
 
 interface BreadcrumbProps {
   breadcrumb: DrupalMenuLinkContent[]
 }
 
 export const Breadcrumb = ({breadcrumb}:BreadcrumbProps): JSX.Element => {
+  const { t } = useTranslation('common')
+
   if (!breadcrumb) return <></>
 
-  const crumbs: ReactElement[] = breadcrumb.map((crumb) => {
+  const crumbs: ReactElement[] = breadcrumb.map((crumb, index) => {
+    if (index == breadcrumb.length -1) {
+      return <li className={styles.breadcrumbElement} key={crumb.id}><span>{crumb.title}</span></li>
+    }
     return (
       <li className={styles.breadcrumbElement} key={crumb.id}>
-        <a href={crumb.url}>{crumb.title}</a>
+        <Link href={crumb.url}><a><span>{crumb.title}</span></a></Link><IconAngleRight size='s' />
       </li>
     )
   })
-  return <ul className={styles.breadcrumb}>{crumbs}</ul>
+
+  return (
+    <div className={classNames(styles.breadcrumb)}>
+      <div className={styles.breadcrumbElement} key='breadcrumb-frontpage'>
+        <Link href="/"><a><span>{t("navigation.frontpage")}</span></a></Link><IconAngleRight size='s' />
+      </div>
+      {crumbs}
+    </div>
+  )
 }
