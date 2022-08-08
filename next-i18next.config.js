49d071171a69bd983a92cc4804e92e8b80adc814
Jussiles <juss.leskinen@gmail.com>
2022-08-08 16:24:37 +0300

    Update next and i18-next packages.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -8,11 +8,8 @@ const locales = languages.map(({ code }) => code)
 
 module.exports = {
   i18n: {
-    reloadOnPrerender: typeof process.env.development !== 'undefined',
     locales,
-    languages,
     defaultLocale: 'fi',
-    fallbackLocale: 'fi',
     localeDetection: false
   }
 }
