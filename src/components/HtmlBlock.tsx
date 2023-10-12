37577f4ff5175e1283be6345a1aaaf5f182b8730
maria.ahokas <maria.ahokas@siili.com>
2023-10-12 16:33:13 +0300

    THF-326: git add HTML muokkaus

--- src/components/HtmlBlock.tsx
+++ src/components/HtmlBlock.tsx
@@ -1,17 +1,34 @@
-import parse from 'html-react-parser'
+import { useEffect } from 'react';
+import parse from 'html-react-parser';
 
-import { DrupalFormattedText } from '@/lib/types'
+import { DrupalFormattedText } from '@/lib/types';
 
 interface HtmlBlockProps {
-  field_text: DrupalFormattedText
+  field_text: DrupalFormattedText;
 }
 
 export function HtmlBlock({ field_text }: HtmlBlockProps): JSX.Element {
+  useEffect(() => {
+    const links = document.getElementsByTagName('a');
+
+    for (let i = 0; i < links.length; i++) {
+      const href = links[i].getAttribute('href');
+
+      if (href && (href.endsWith('.doc') || href.endsWith('.pdf'))) {
+        links[i].removeAttribute('data-is-external');
+        if (links[i] instanceof Element) {
+          const parentElement = links[i].parentNode as HTMLElement;
+          parentElement?.classList.add('modified-before');
+        }
+      }
+    }
+  }, []);
+
   return (
     <div className="block-text">
       {parse(field_text?.processed)}
     </div>
-  )
+  );
 }
 
-export default HtmlBlock
+export default HtmlBlock;
